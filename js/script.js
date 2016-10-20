dfde2dd8e052449f980be6567f7a9b6448da288c
Sang Min Na <xsangmin@gmail.com>
2016-10-19 21:34:56 -0400

    add waypoint and sticky library

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(() => {
   console.log('script loaded');
 
+  const waypoint = new Waypoint({
+    element: document.getElementById('basic-waypoint'),
+    handler: () => {
+      notify('Basic waypoint triggered')
+    },
+  });
+
+  
+
 })
