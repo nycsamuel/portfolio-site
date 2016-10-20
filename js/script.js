6322c1805707448d0951a17d145d6027dc37c33b
Sang Min Na <xsangmin@gmail.com>
2016-10-19 22:22:39 -0400

    test waypoint methods for scrolling effect page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 $(document).ready(() => {
   console.log('script loaded');
 
-  const waypoint = new Waypoint({
-    element: document.getElementById('basic-waypoint'),
-    handler: () => {
-      notify('Basic waypoint triggered')
-    },
+  /* this doesn't work with fat arrow function! */
+
+  const firstWaypoint = $('#first').waypoint(direction => {
+    console.log('first point => ' + direction);
   });
+  const secondWaypoint = $('#second').waypoint(direction => {
+    console.log('second poingt => ' + direction);
+  }, { offset: '25%'});
+  const thirdWaypoint = $('#third').waypoint(direction => {
+    console.log('third poingt => ' + direction);
+  }, { offset: '25%'});
 
-  
 
-})
+
+}) // ready
