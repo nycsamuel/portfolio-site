bdf21c064d01b7deffecad67f5f7c130c8c4673b
Sang Min Na <xsangmin@gmail.com>
2016-11-06 09:36:42 -0500

    change positions of icons

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// DOM loaded
+$(document).ready(() => {
+  console.log('js connected!');
+
+  const aboutmeBtn = $('#about');
+  const projectBtn = $('#list-projects');
+
+  aboutmeBtn.on('click', () => {
+    // console.log('about me clicked');
+    
+  });
+
+}); // doc ready
