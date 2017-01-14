7ed7a04b067a682f6658d87ef9a82ab56d72fd1f
Sang Min Na <xsangmin@gmail.com>
2017-01-13 22:51:00 -0500

    update

--- js/init.js
+++ js/init.js
@@ -1,7 +1,13 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+      }
+    );
     $('.slider').slider({full_width: false});
     $('.collapsible').collapsible();
     $(".dropdown-button").dropdown();
