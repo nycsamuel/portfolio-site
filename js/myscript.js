e20fed3934ce4f5d798fd7b5849f0b149cdb518f
nycsamuel <xsangmin@gmail.com>
2017-04-15 03:37:14 -0400

    add hamburger icon and nav dropdown menu

--- js/myscript.js
+++ js/myscript.js
@@ -2,13 +2,15 @@ $(document).ready(() => {
 
 
 	console.log('loaded')
-	const navBtn  = $('div.nav-btn')
-	const menu = $('div.menu')
+	const navBtn = $('div.nav-btn')
+	const menu   = $('div.menu-container')
+
 
 	navBtn.on('click', (event) => {
 		console.log('clicked nav')
 		navBtn.toggleClass('clicked')
-		menu.toggleClass('show-menu')
+		menu.toggleClass('show')
 	})
+
 	
 })
