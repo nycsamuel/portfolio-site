8b64abdd3ef38c88da87bef1e13389d9af3b644d
nycsamuel <xsangmin@gmail.com>
2017-04-17 23:45:35 -0400

    move .main body when side navigator is activated

--- js/myscript.js
+++ js/myscript.js
@@ -2,15 +2,22 @@ $(document).ready(() => {
 
 
 	console.log('loaded')
-	const navBtn   = $('div.nav-btn')
-	const dropdown = $('div.dropdown-container')
+	const navBtn     = $('div.nav-btn')
+	const dropdown   = $('div.dropdown-container')
 	const sideDrawer = $('div.side-drawer')
+	const main       = $('div.main')
 
 	navBtn.on('click', (event) => {
 		console.log('clicked nav')
 		navBtn.toggleClass('clicked')
 		// dropdown.toggleClass('show')
 		sideDrawer.toggleClass('drawer-active')
+		if (sideDrawer.hasClass('drawer-active')) { 
+			main.css('margin-left', '30%' ) 
+		} else {
+			main.css('margin-left', '0' )
+		}
+
 	})
 	
 	// enable all tooltips
