effabd66f9a16052e5c8905de3eba50d49ca6157
nycsamuel <xsangmin@gmail.com>
2017-05-30 18:42:12 -0400

    fade effects when clicked

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,17 @@
 $(document).ready(() => {
-	console.log('hey');	
+	console.log('hi :)');	
 
 	$('.nav-tabs li').on('click', function(event) {
+    // nav tabs
+    // console.log($(this).text())
     $(this).siblings().removeClass('active');
     $(this).addClass('active');
+    
+    // descriptions containers
+    $('div.active').fadeOut('slow');
+    $('div.active').removeClass('active');
+    $(`div#${$(this).text().toLowerCase()}`).fadeIn('slow').addClass('active');
+    // console.log($(`div#${$(this).text().toLowerCase()}`))
   })
 
 })
