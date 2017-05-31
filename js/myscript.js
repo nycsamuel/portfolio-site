e2bcb0e5ef5d68f898f2779de22620c32051b8ac
nycsamuel <xsangmin@gmail.com>
2017-05-30 21:27:14 -0400

    name change

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
 	console.log('hi :)');	
 
-	$('.nav-tabs li').on('click', function(event) {
+  $('.nav-tabs li').on('click', function(event) {
     // nav tabs
     // console.log($(this).text())
     $(this).siblings().removeClass('active');
@@ -12,6 +12,21 @@ $(document).ready(() => {
     $('div.active').fadeOut('slow');
     $('div.active').removeClass('active');
     $(`div#${$(this).text().toLowerCase()}`).fadeIn('slow').addClass('active');
-  })
+  });
   
+  let intervalId = setInterval(function() {
+    let name = $('span.name');
+    for (let i = 0; i < name.length; i++) {
+      let r = Math.round(Math.random()*255);
+      let g = Math.round(Math.random()*255);
+      let b = Math.round(Math.random()*255);
+      $(name[i]).css('color', `rgb(${r}, ${g}, ${b})`);
+    }  
+  }, 1000);  
+  // let intervalId = setInterval(function() {
+  //   let r = Math.round(Math.random()*255);
+  //   let g = Math.round(Math.random()*255);
+  //   let b = Math.round(Math.random()*255);
+  //   $('#main > h1').css('background-color', `rgb(${r}, ${g}, ${b})`);
+  // }, 1000);
 })
